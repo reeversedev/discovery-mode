28320c1e2328bb6f8b6e95eaecd289a78312c9c0
Prateek Gogia <prateek_gogia@gridle.io>
2018-11-21 11:58:01 +0530

    Added Transistions and autocomplete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactMapGL, {FlyToInterpolator} from "react-map-gl";
+import "./index";
+import Dropdown from "./Dropdown";
+require("dotenv").config();
+
+const geocodingUrl = "https://api.mapbox.com/geocoding/v5";
+const mapboxGeocoding = query =>
+  `${geocodingUrl}/mapbox.places/${query}.json?access_token=${
+    process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
+  }`;
 
 class App extends Component {
+  state = {
+    viewport: {
+      width: 2000,
+      height: 1000,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 14
+    },
+    options: []
+  };
+  makeQuery = query => {
+    fetch(mapboxGeocoding(query))
+      .then(res => res.json())
+      .then(data =>
+        data.features
+          ? this.setState({
+              options: data.features
+            })
+          : this.setState({ options: "" })
+      );
+  };
+  onSearchItem = index => {
+    console.log(index);
+    console.log(this.state.options);
+    this.setState({
+      viewport: {
+        width: 2000,
+        height: 1000,
+        longitude: this.state.options[index].center[0],
+        latitude: this.state.options[index].center[1],
+        zoom: 14
+      }
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Dropdown
+          makeQuery={this.makeQuery}
+          options={this.state.options}
+          onSearchItem={this.onSearchItem}
+        />
+        <ReactMapGL
+          {...this.state.viewport}
+          onViewportChange={viewport => this.setState({ viewport })}
+          transitionDuration={5000}
+          transitionInterpolator={new FlyToInterpolator()}
+          // onViewStateChange={viewport => console.log("viewport", viewport)}
+        />
+      </>
     );
   }
 }
---
+++ src/Dropdown.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Input = styled.input`
+  width: 100%;
+  height: 100%;
+  border: 1px solid #ddd;
+  outline: none;
+  padding: 0px 20px;
+  color: #666;
+  font-size: 13px;
+  ::placeholder {
+    color: #bbb;
+  }
+`;
+
+const Container = styled.div`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 40px;
+  background-color: white;
+  flex: 1;
+`;
+
+const List = styled.ul`
+  list-style-type: none;
+  padding: 6px 20px;
+  background-color: white;
+  margin-top: 0px;
+`;
+
+const Item = styled.li`
+  padding: 4px 0px;
+  color: #666;
+  font-size: 13px;
+  cursor: pointer;
+`;
+
+export default class Dropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+  onSearch = async query => {
+    this.setState({
+      value: query
+    });
+    if (query && query.length > 2) {
+      await this.props.makeQuery(query);
+    }
+  };
+  onSelectItem = async index => {
+    await this.props.onSearchItem(index);
+  };
+  render() {
+    const { options } = this.props;
+    const { value } = this.state;
+
+    return (
+      <Container>
+        <Input
+          onChange={e => this.onSearch(e.target.value)}
+          value={value}
+          placeholder="Search for a venue"
+        />
+        {value.length > 2 && (
+          <List>
+            {options.map((el, index) => (
+              <Item key={index} onClick={this.onSelectItem.bind(this, index)}>
+                {el.place_name}
+              </Item>
+            ))}
+          </List>
+        )}
+      </Container>
+    );
+  }
+}
